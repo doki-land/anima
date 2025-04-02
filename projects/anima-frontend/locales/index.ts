60a4872e84accbc573917422166c7f5d8417a42d
Aster <galaster@foxmail.com>
2025-04-02 19:27:35 +0800

    Add missing index.html as entrypoint

--- projects/anima-frontend/locales/index.ts
+++ projects/anima-frontend/locales/index.ts
@@ -1,12 +1,12 @@
-import { createFluentVue } from 'fluent-vue'
-import { FluentBundle, FluentResource } from '@fluent/bundle'
-import { negotiateLanguages } from '@fluent/langneg'
+import {createFluentVue} from 'fluent-vue'
+import {FluentBundle, FluentResource} from '@fluent/bundle'
+import {negotiateLanguages} from '@fluent/langneg'
 
 const supportedLocales = ['en-US', 'zh-HANS', 'zh-HANT']
 const defaultLocale = 'zh-HANS'
 
 const userLocales = navigator.languages || [defaultLocale]
-const currentLocale = negotiateLanguages(userLocales, supportedLocales, { defaultLocale })[0]
+const currentLocale = negotiateLanguages(userLocales, supportedLocales, {defaultLocale})[0]
 
 const bundle = new FluentBundle(currentLocale)
 const resource = new FluentResource(`
@@ -15,7 +15,7 @@ hello = Hello, world!
 bundle.addResource(resource)
 
 const fluentVue = createFluentVue({
-  bundles: [bundle]
+    bundles: [bundle]
 })
 
 export default fluentVue
--- projects/anima-frontend/router.ts
+++ projects/anima-frontend/router.ts
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Home from '@/pages/Home.vue'
 import World from '@/pages/World.vue'
 import Avatar from '@/pages/Avatar.vue'
 import Group from '@/pages/Group.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/w/:world_id',
-    name: 'World',
-    component: World
-  },
-  {
-    path: '/a/:avtar_id',
-    name: 'Avatar',
-    component: Avatar
-  },
-  {
-    path: '/g/:group_id',
-    name: 'Group',
-    component: Group
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/w/:world_id',
+        name: 'World',
+        component: World
+    },
+    {
+        path: '/a/:avtar_id',
+        name: 'Avatar',
+        component: Avatar
+    },
+    {
+        path: '/g/:group_id',
+        name: 'Group',
+        component: Group
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
